7ee79f37d342c09f5815905ec4fb41290947c7cc
Anusuya-SK <anusuyak98.ak@gmail.com>
2023-11-28 11:34:53 +0530

    Initial Commit

---
+++ inputoutputdemo.java
@@ -0,0 +1,32 @@
+/********* Getting input from User *************/
+
+// Import the Scanner from inbuilt packages "util"
+import java.util.Scanner;
+
+public class inputdemo {
+    public static void main(String args[]) {
+        // int a; int-datatype, a-variable
+        // Scanner scanner; Scanner-class, scanner-object
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        // Scanning the next line and store in a string called "name"
+        String name = Scanner.nextLine();
+
+        System.out.println("Rate us from 1 to 5");
+        // Scanning the nextInt and store in an integer called "rating"
+        int rating = Scanner.nextInt();
+        // short rating = Scanner.nextShort();
+        // After scan the integer, we give enter that enter take as a string and set to Email. So we are scanning the empty line and storing it anywhere.
+        Scanner.nextLine();
+
+        System.out.println("Enter the EmailId");
+        // Scanning the next line and store in a string called "email"
+        String email = Scanner.nextLine();
+
+
+        System.out.println("Hello" + name);
+        System.out.println("Your rating to us is" + rating);
+        System.out.println("Your EmailId" + email);
+    } 
+}
